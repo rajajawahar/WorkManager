f294c454df91662ff198a7b06451c1bc482464ef
Raja Jawahar <rajajawahar77@gmail.com>
2023-01-29 12:10:37 +0530

    OneTime Request action completd

--- app/src/main/java/com/tarkalabs/workmanager/MainActivity.kt
+++ app/src/main/java/com/tarkalabs/workmanager/MainActivity.kt
@@ -4,15 +4,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
+import com.tarkalabs.workmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+    binding.btnTrigger.setOnClickListener {
+      callMyFirstWorker()
+    }
   }
 
   private fun callMyFirstWorker() {
-    val request = OneTimeWorkRequest.Builder(MyOneTimeWorker::class.java).build()
+    val request =
+      OneTimeWorkRequest.Builder(MyOneTimeWorker::class.java).build()
     WorkManager.getInstance(this).enqueue(request)
   }
 }
